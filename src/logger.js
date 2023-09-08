d86fba80040a0b31cdd5e8ca0a13db96b09c6146
cmrxnn <cmrxn2005@gmail.com>
2023-09-08 23:03:50 +0100

    display accurate time/date in logs

--- src/logger.js
+++ src/logger.js
@@ -1,21 +1,19 @@
 const colors = require('colors')
 
-const date = `${colors.bold.gray(new Date().toLocaleDateString())} ${colors.gray(new Date().toLocaleTimeString())}`
-
 const success = (message) => {
-  return console.info(`${date} | ${colors.bold.green('  DONE  ')} | ${message}`)
+  return console.info(`${colors.bold.gray(new Date().toLocaleDateString())} ${colors.gray(new Date().toLocaleTimeString())} | ${colors.bold.green('  DONE  ')} | ${message}`)
 }
 
 const info = (message) => {
-  return console.info(`${date} | ${colors.bold.cyan('  INFO  ')} | ${message}`)
+  return console.info(`${colors.bold.gray(new Date().toLocaleDateString())} ${colors.gray(new Date().toLocaleTimeString())} | ${colors.bold.cyan('  INFO  ')} | ${message}`)
 }
 
 const warn = (message) => {
-  return console.info(`${date} | ${colors.bold.yellow('  WARN  ')} | ${message}`)
+  return console.info(`${colors.bold.gray(new Date().toLocaleDateString())} ${colors.gray(new Date().toLocaleTimeString())} | ${colors.bold.yellow('  WARN  ')} | ${message}`)
 }
 
 const error = (message) => {
-  return console.info(`${date} ${colors.red('!')} ${colors.bold.red('  ERROR  ')} ${colors.red('!')} ${message}`)
+  return console.info(`${colors.bold.gray(new Date().toLocaleDateString())} ${colors.gray(new Date().toLocaleTimeString())} ${colors.red('!')} ${colors.bold.red('  ERROR  ')} ${colors.red('!')} ${message}`)
 }
 
 module.exports = { success, info, warn, error }
