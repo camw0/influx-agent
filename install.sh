506913a50d39a6590923215f9c5b25b8fc4e5504
cmrxnn <cmrxn2005@gmail.com>
2023-09-28 22:14:50 +0100

    create install script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# You need to set these variables before running the install script.
+export GIT_NAME=
+export GIT_TOKEN=
+# You need to set these variables before running the install script.
+
+sudo apt update;
+sudo apt -y install nodejs npm;
+sudo npm i -g yarn pkg;
+
+git pull https://$GIT_NAME:$GIT_TOKEN@github.com/cmrxnn/influx-agent;
+
+cd influx-agent;
+yarn install;
+yarn build;
+
+sudo mv influx-agent /usr/local/bin;
+sudo mkdir -p /var/lib/influx-agent;
+sudo mkdir -p /etc/influx-agent;
+sudo cp -R settings.json.example /etc/influx-agent;
+
+sudo echo "
+[Unit]
+Description=InfluxAgent
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/influx-agent
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/influx-agent.service;
+
+systemctl start influx-agent --now;
+
+exit 0;
